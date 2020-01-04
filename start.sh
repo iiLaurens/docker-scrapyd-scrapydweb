9a68eb37f5acc995742bc0b2d92e92f74b24f344
iiLaurens <iiLaurens@users.noreply.github.com>
2020-01-04 23:24:53 +0100

    Update start.sh
    Allow access by everything

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 # Copy config file to directory if no config found
 mkdir -p /home/scrapydweb/projects
+chmod -R 777 /home/scrapydweb
 cp -n /usr/local/scrapydweb_settings_v10.py /home/scrapydweb/scrapydweb_settings_v10.py
 rm -f /home/scrapydweb/twistd.pid
 
