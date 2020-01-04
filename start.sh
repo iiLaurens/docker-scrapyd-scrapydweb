5a16f45e97cb80568b8aa56291e014600b87fbb3
iiLaurens <iiLaurens@users.noreply.github.com>
2020-01-04 23:25:27 +0100

    Update start.sh
    Delay chmod command

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 # Copy config file to directory if no config found
 mkdir -p /home/scrapydweb/projects
-chmod -R 777 /home/scrapydweb
 cp -n /usr/local/scrapydweb_settings_v10.py /home/scrapydweb/scrapydweb_settings_v10.py
+chmod -R 777 /home/scrapydweb
 rm -f /home/scrapydweb/twistd.pid
 
 scrapyd &
