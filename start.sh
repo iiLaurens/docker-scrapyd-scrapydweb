68ab0c6cc95af919eff892569f9cbc231f74361a
iiLaurens <iiLaurens@users.noreply.github.com>
2020-01-04 23:16:17 +0100

    Update start.sh
    Remove twistd.pid file after restart to prevent clash

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # Copy config file to directory if no config found
 mkdir -p /home/scrapydweb/projects
 cp -n /usr/local/scrapydweb_settings_v10.py /home/scrapydweb/scrapydweb_settings_v10.py
+rm -f /home/scrapydweb/twistd.pid
 
 scrapyd &
 sleep 2
