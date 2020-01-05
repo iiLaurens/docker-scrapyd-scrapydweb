7e7e792fc894a96f00261964ad38b0d717862c71
iiLaurens <iiLaurens@users.noreply.github.com>
2020-01-05 13:29:44 +0100

    Update start.sh
    Ensure scrapyd directory structure is initialized

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Ensure scrapyd directory structure is initialized
+mkdir -p /var/lib/scrapyd/eggs
+mkdir -p /var/lib/scrapyd/logs
+mkdir -p /var/lib/scrapyd/items
+mkdir -p /var/lib/scrapyd/dbs
+
 # Copy config file to directory if no config found
 mkdir -p /home/scrapydweb/projects
 cp -n /usr/local/scrapydweb_settings_v10.py /home/scrapydweb/scrapydweb_settings_v10.py
